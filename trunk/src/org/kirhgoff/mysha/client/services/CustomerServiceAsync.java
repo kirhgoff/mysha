0ff1af15a0a97fab2815a2392c97c1a9310402eb
kirill.lastovirya <kirill.lastovirya@18ddefcc-c747-11dd-9042-6d746b85d38b>
2009-01-06 06:03:27 +0000

    added CustomerDAO and log4j support

--- trunk/src/org/kirhgoff/mysha/client/services/CustomerServiceAsync.java
+++ trunk/src/org/kirhgoff/mysha/client/services/CustomerServiceAsync.java
@@ -1,9 +1,11 @@
 package org.kirhgoff.mysha.client.services;
 
+import java.util.List;
+
 import org.kirhgoff.mysha.domain.Customer;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface CustomerServiceAsync {
-	void getCustomers (AsyncCallback<Customer[]> callback);
+	void getCustomers (AsyncCallback<List<Customer>> callback);
 }
